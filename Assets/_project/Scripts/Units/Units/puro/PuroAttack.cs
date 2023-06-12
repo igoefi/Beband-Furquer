1ec6c3b520c0928cd1048c828e24971165433593
igoefi <igoefi@mail.ru>
2023-06-12 22:44:29 +0300

    AddNormal3DModels

---
+++ Assets/_project/Scripts/Units/Units/puro/PuroAttack.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuroAttack : MonoBehaviour
+{
+    [SerializeField] List<Transform> _placesForRockets;
+    [SerializeField] Rocket _rocketPrefab;
+
+    [SerializeField] Transform _transformToLookAt;
+
+    private Coroutine _attackCorutine;
+    private UnitStats _stats;
+    private UnitLogic _logic;
+
+    private void Start()
+    {
+        _stats = GetComponent<UnitStats>();
+        _logic = GetComponent<UnitLogic>();
+
+        _logic.IsCameToEnemy.AddListener(StartAttack);
+        _logic.IsStopAttackingEnemy.AddListener(EndAttack);
+    }
+
+    private void StartAttack(IDamagable enemy) =>
+        _attackCorutine = StartCoroutine(Attack(enemy));
+
+    private void EndAttack()
+    {
+        if (_attackCorutine != null)
+            StopCoroutine(_attackCorutine);
+    }
+
+    private IEnumerator Attack(IDamagable enemy)
+    {
+        yield return new WaitWhile(() => !_stats.IsReadyToAction());
+
+        if (_transformToLookAt != null)
+        {
+            _transformToLookAt.LookAt(enemy.GetPosition());
+        }
+        if (enemy != null)
+            foreach (var place in _placesForRockets)
+            {
+                var obj = Instantiate(_rocketPrefab.gameObject, place.position, transform.rotation, transform.parent)
+                    .GetComponent<Rocket>();
+                obj.SetTarget(enemy.GetPosition(), _stats.GetDamageToAttack(), _stats.IsEnemy());
+            }
+
+        _logic.SetActiveFalse(true);
+    }
+}
